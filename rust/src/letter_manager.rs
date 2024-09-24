8f7b43258d731c493408cd2a123ad17081c872c3
Jade Lynn Masker <donottellmetonottellyou@gmail.com>
2024-09-23 23:31:17 -0400

    refactor ExtLetterManager & rename nodes
    Renamed:
    LetterBox -> TraySlots
    WordBoxes -> WordSlots
    
    The code is much more concise and readable, no opaque error types.

--- rust/src/letter_manager.rs
+++ rust/src/letter_manager.rs
@@ -1,135 +1,60 @@
 use crate::letters::ExtLetter;
 
-use anyhow::{anyhow, Context, Result};
 use godot::prelude::*;
-use thiserror::Error;
 
 use std::ops::{Deref, DerefMut};
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, Error)]
-pub enum LetterManagerError {
-    #[error("Not loaded due to not being in scene tree")]
-    NotInitialized,
-    #[error("{0} not found in correct location")]
-    NotFound(&'static str),
-    #[error("This is not a valid slot in the current tree")]
-    NotPartOfTree,
-    #[error("No letter here")]
-    NoLetter,
-}
-
 #[derive(GodotClass)]
-#[class(base=Node2D)]
+#[class(base=Node2D, init)]
 pub struct ExtLetterManager {
-    word: [Result<Gd<ExtLetter>>; 5],
-    letters: [Result<Gd<ExtLetter>>; 10],
-    word_slots: [Result<Gd<Node2D>>; 5],
-    letters_slots: [Result<Gd<Node2D>>; 10],
-
     base: Base<Node2D>,
+
+    word_slots: FixedVec<Gd<Node2D>>,
+    word_letters: FixedVec<Option<Gd<ExtLetter>>>,
+    tray_slots: FixedVec<Gd<Node2D>>,
+    tray_letters: FixedVec<Option<Gd<ExtLetter>>>,
 }
 #[godot_api]
 impl INode2D for ExtLetterManager {
-    fn init(base: Base<Self::Base>) -> Self {
-        use LetterManagerError::NotInitialized;
-
-        let word = std::array::from_fn(|_| Err(NotInitialized.into()));
-        let letters = std::array::from_fn(|_| Err(NotInitialized.into()));
-        let word_slots = std::array::from_fn(|_| Err(NotInitialized.into()));
-        let letters_slots = std::array::from_fn(|_| Err(NotInitialized.into()));
-
-        Self {
-            word,
-            letters,
-            word_slots,
-            letters_slots,
-
-            base,
-        }
-    }
-
     fn ready(&mut self) {
-        use LetterManagerError::{NoLetter, NotFound, NotPartOfTree};
+        self.word_slots = (0_u8..)
+            .map_while(|i| self.base().try_get_node_as(format!("WordSlots/{i}")))
+            .collect::<Vec<_>>()
+            .into();
+        let word_slots_len = self.word_slots.len();
+        self.word_letters = vec![None; word_slots_len].into();
 
-        let word_boxes: Result<Gd<Node2D>, _> = self
-            .base()
-            .try_get_node_as("WordBoxes")
-            .ok_or(NotFound("WordBoxes"));
-        let letter_box: Result<Gd<Node2D>, _> = self
-            .base()
-            .try_get_node_as("LetterBox")
-            .ok_or(NotFound("LetterBox"));
+        self.tray_slots = (0_u8..)
+            .map_while(|i| self.base().try_get_node_as(format!("TraySlots/{i}")))
+            .collect::<Vec<_>>()
+            .into();
+        let tray_slots_len = self.tray_slots.len();
+        self.tray_letters = self
+            .tray_slots
+            .iter()
+            .map(|node| node.try_get_node_as("ExtLetter"))
+            .inspect(|letter| match letter {
+                Some(_) => (),
+                None => godot_error!("An available tray slot is missing a letter!"),
+            })
+            .collect::<Vec<_>>()
+            .into();
 
-        let (found_word_slots, found_letters_slots) = match (word_boxes, letter_box) {
-            (Ok(word_boxes), Ok(letter_box)) => {
-                (word_boxes.get_children(), letter_box.get_children())
-            }
-            (Err(e), Ok(_)) | (Ok(_), Err(e)) => {
-                for slot in self
-                    .word_slots
-                    .iter_mut()
-                    .chain(self.letters_slots.iter_mut())
-                {
-                    *slot = Err(e).context("Parent was not found");
-                }
-
-                return godot_error!("{e}");
-            }
-            (Err(e1), Err(e2)) => {
-                for slot in self
-                    .word_slots
-                    .iter_mut()
-                    .chain(self.letters_slots.iter_mut())
-                {
-                    *slot = Err(e1).with_context(|| e2).context("Parent was not found");
-                }
-
-                return godot_error!("{e1}\n{e2}");
-            }
-        };
-
-        let mut found_word_slots = found_word_slots.iter_shared();
-        for (word_slot, letter) in self.word_slots.iter_mut().zip(self.word.iter_mut()) {
-            match found_word_slots.next() {
-                Some(found_word_slot) => {
-                    *word_slot = found_word_slot
-                        .try_cast::<Node2D>()
-                        .map_err(|e| anyhow!("{e}"))
-                        .context("Child should be a Node2D")
-                        .inspect_err(|e| godot_error!("{e}"));
-                    *letter = match word_slot {
-                        Ok(_) => Err(NoLetter.into()),
-                        Err(e) => Err(anyhow!("{e}")).context("Parent was not found"),
-                    }
-                }
-                None => *word_slot = Err(NotPartOfTree.into()),
-            }
+        if word_slots_len == 0 || tray_slots_len == 0 {
+            godot_error!("Either word or letters has a length of zero");
         }
-
-        let mut found_letters_slots = found_letters_slots.iter_shared();
-        for (letters_slot, letter) in self.letters_slots.iter_mut().zip(self.letters.iter_mut()) {
-            match found_letters_slots.next() {
-                Some(found_letters_slot) => {
-                    *letters_slot = found_letters_slot
-                        .try_cast::<Node2D>()
-                        .map_err(|e| anyhow!("{e}"))
-                        .context("Child should be a Node2D")
-                        .inspect_err(|e| godot_error!("{e}"));
-                    *letter = match letters_slot {
-                        Ok(letters_slot) => {
-                            letters_slot.try_get_node_as("ExtLetter").ok_or(anyhow!(
-                            "LettersSlot nodes should have one ExtLetter child in the scene tree"
-                        ))
-                        }
-                        Err(e) => Err(anyhow!("{e}").context("Parent was not found")),
-                    };
-                }
-                None => {
-                    *letters_slot = Err(NotPartOfTree.into());
-                    *letter = Err(NotPartOfTree.into());
-                }
-            }
+        if word_slots_len > tray_slots_len {
+            godot_error!(
+                "Word length {} is greater than {} available letters",
+                word_slots_len,
+                tray_slots_len
+            );
         }
+        godot_print!(
+            "Initialized ExtLetterManager with {} letters for a {} length word",
+            tray_slots_len,
+            word_slots_len
+        );
     }
 }
 
