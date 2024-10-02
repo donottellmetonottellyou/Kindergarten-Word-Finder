4308708996207b3b10665b8161428c4b1417738f
Jade Lynn Masker <donottellmetonottellyou@gmail.com>
2024-10-01 20:22:54 -0400

    make EditorRunBehavior::AllClasses

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,8 +2,12 @@ pub mod letters;
 pub mod managers;
 pub mod words;
 
-use godot::prelude::*;
+use godot::{init::EditorRunBehavior, prelude::*};
 
 struct KindergartenWordFinderExtensionLibrary;
 #[gdextension]
-unsafe impl ExtensionLibrary for KindergartenWordFinderExtensionLibrary {}
+unsafe impl ExtensionLibrary for KindergartenWordFinderExtensionLibrary {
+    fn editor_run_behavior() -> EditorRunBehavior {
+        EditorRunBehavior::AllClasses
+    }
+}
